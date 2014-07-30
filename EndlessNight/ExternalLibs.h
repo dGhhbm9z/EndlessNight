72289ddb270a946813b794c3f601dbe5a331c66e
thanosCebolitas <thanos@accusonus.com>
2014-07-31 02:02:47 +0300

    Project changes in Xcode

--- EndlessNight/ExternalLibs.h
+++ EndlessNight/ExternalLibs.h
@@ -2,7 +2,7 @@
 #ifdef __APPLE__
     #include <SDL2/SDL.h>
     #include <SDL2_image/SDL_image.h>
-	#include <SDL2_image/SDL_ttf.h>
+	#include <SDL2/SDL_ttf.h>
 #else
     #include <SDL.h>
     #include <SDL_image.h>
